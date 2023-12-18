f0d49d5c026d088960c339c0ed0fc34e7b505d7e
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2023-12-18 16:01:33 +1100

    added functionality related to flags

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ window.onload = function () {
 
 function startGame() {
   document.querySelector("#mines-count").innerText = minesCount;
+  document.querySelector("#flag-button").addEventListener("click", setFlag);
 
   for (let r = 0; r < rows; r++) {
     let row = [];
@@ -21,6 +22,7 @@ function startGame() {
       let tile = document.createElement("div");
       tile.id = `${r}-${c}`;
       tile.className = "board__tile";
+      tile.addEventListener("click", clickTile);
       document.querySelector("#board").appendChild(tile);
       row.push(tile);
     }
@@ -29,4 +31,26 @@ function startGame() {
   }
 
   console.log(board);
+}
+
+function setFlag() {
+  if (flagEnabled) {
+    flagEnabled = false;
+    document.querySelector("#flag-button").style.backgroundColor = "lightgray";
+  } else {
+    flagEnabled = true;
+    document.querySelector("#flag-button").style.backgroundColor = "darkgray";
+  }
+}
+
+function clickTile() {
+  let tile = this;
+
+  if (flagEnabled) {
+    if (tile.innerText == "") {
+      tile.innerText = "🚩";
+    } else if (tile.innerText == "🚩") {
+      tile.innerText = "";
+    }
+  }
 }
